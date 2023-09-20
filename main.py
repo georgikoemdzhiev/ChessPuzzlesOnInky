dde2fe2e0f4934090a644afe774cc4453aeef939
Georgi Koemdzhiev <koemdjiev@gmail.com>
2023-09-20 19:37:18 +0100

    WIP

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import io
 import chess
 import chess.svg
 import chess.engine
+import cairosvg
 from PIL import Image, ImageDraw
 
 def uci_to_san(uci_moves, initial_position_fen):
@@ -16,15 +17,45 @@ def uci_to_san(uci_moves, initial_position_fen):
 
     return san_moves, board
 
+def png_to_bmp(png_file, output_file):
+    try:
+        # Open the PNG image
+        with Image.open(png_file) as img:
+            # Convert and save it as BMP
+            img.save(output_file, 'BMP')
+        
+        return True
+    except Exception as e:
+        print(f"Error converting PNG to BMP: {str(e)}")
+        return False
+    
+def svg_to_png(svg_content, output_file):
+    try:
+        # Convert SVG content to a PNG in memory
+        png_data = cairosvg.svg2png(bytestring=svg_content)
+
+        # Convert PNG data to a PIL Image object
+        # with Image.open(io.BytesIO(png_data)) as img:
+        #     img.save(output_file, 'PNG')
+    
+        png_to_bmp(png_data, "output.bmp")
+        return True
+    except Exception as e:
+        print(f"Error converting SVG to PNG: {str(e)}")
+        return False
+    
+
 def generate_chessboard_image(board):
     # Create an empty chessboard image
     board_image = chess.svg.board(board=board)
-    with open('temp.svg', 'w') as outputfile:
-        outputfile.write(board_image)
+    # with open('board_image.svg', 'w') as outputfile:
+    #     outputfile.write(board_image)
+    svg_to_png(board_image,"output.png")
     # TODO write logic to convert SVG to BMP
     # # Convert the SVG image to BMP using PIL
     # with Image.open(io.BytesIO(board_image.encode('utf-8'))) as img:
     #     img.save("chessboard.bmp", "BMP")
+
 
 # Example usage
 uci_moves = "e2e4 e7e5 g1f3"
