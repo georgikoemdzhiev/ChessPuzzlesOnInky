a2bcab76d4ee9b2854936f2afdd70d92ec089410
Georgi Koemdzhiev <koemdjiev@gmail.com>
2023-09-27 19:49:43 +0100

    added display func; refactoring

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ import cairosvg
 from PIL import Image
 from inky.auto import auto
 
+WHITE = (255,255,255)
+
+def display_image(image: Image):
+    inky = auto() # requires i2c interface enabled
+    blank_img = Image.new('RGB', inky.resolution,WHITE)
+    resized_img = image.resize(480,480) # make room for other content
+    blank_img.paste(resized_img, (0,0))
+    inky.set_image(blank_img)
+    inky.show()
+
 def uci_to_san(uci_moves, initial_position_fen):
     board = chess.Board(initial_position_fen)
     san_moves = []
@@ -17,31 +27,15 @@ def uci_to_san(uci_moves, initial_position_fen):
         san_moves.append(san_move)
 
     return san_moves, board
-
-def png_to_bmp(png_file, output_file):
-    try:
-        # Open the PNG image
-        with Image.open(png_file) as img:
-            # Convert and save it as BMP
-            img.save(output_file, 'BMP')
-        
-        return True
-    except Exception as e:
-        print(f"Error converting PNG to BMP: {str(e)}")
-        return False
     
-def svg_to_png(svg_content, output_file):
+def svg_to_png(svg_content):
     try:
         # Convert SVG content to a PNG in memory
         png_data = cairosvg.svg2png(bytestring=svg_content)
 
         # Convert PNG data to a PIL Image object
         with Image.open(io.BytesIO(png_data)) as img:
-            display = auto()
-            display.set_image(img)
-            img.save(output_file, 'PNG')
-    
-        png_to_bmp("output.png", "output.bmp")
+            display_image(img)
         return True
     except Exception as e:
         print(f"Error converting SVG to PNG: {str(e)}")
@@ -51,8 +45,7 @@ def svg_to_png(svg_content, output_file):
 def generate_chessboard_image(board):
     # Create an empty chessboard image
     board_image = chess.svg.board(board=board)
-    svg_to_png(board_image,"output.png")
-
+    svg_to_png(board_image)
 
 # Example usage
 # uci_moves = "e8d7 a2e6 d7d8 f7f8"
