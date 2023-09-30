d6d92b9515c6f40b3337d529c786ec71f1fe6837
Georgi Koemdzhiev <koemdjiev@gmail.com>
2023-09-30 10:45:26 +0100

    code refactoring; added logic to display image when the inky is not connected

--- main.py
+++ main.py
@@ -5,17 +5,28 @@ import chess.engine
 import cairosvg
 from PIL import Image
 from inky.auto import auto
+from inky.eeprom import read_eeprom
 
 WHITE = (255,255,255)
 PUZZLE_IMAGE_SIZE = (480,480)
 
+def is_inky_connected():
+    return read_eeprom() != None
+
 def display_image(image: Image):
-    inky = auto() # requires i2c interface enabled
-    blank_img = Image.new('RGB', inky.resolution,WHITE)
+    resolution = (800,480)
+    blank_img = Image.new('RGB', resolution,WHITE)
     resized_img = image.resize(PUZZLE_IMAGE_SIZE) # make room for other content
     blank_img.paste(resized_img, (0,0))
-    inky.set_image(blank_img)
-    inky.show()
+
+    if(is_inky_connected()):
+        inky = auto() # requires i2c interface enabled
+        resolution = inky.resolution
+        inky.set_image(blank_img)
+        inky.show()
+    else:    
+        blank_img.show()
+
 
 def uci_to_san(uci_moves, initial_position_fen):
     board = chess.Board(initial_position_fen)
@@ -29,24 +40,21 @@ def uci_to_san(uci_moves, initial_position_fen):
 
     return san_moves, board
     
-def svg_to_png(svg_content):
+def svg_to_png(svg_content) -> Image:
     try:
         # Convert SVG content to a PNG in memory
         png_data = cairosvg.svg2png(bytestring=svg_content)
 
         # Convert PNG data to a PIL Image object
-        with Image.open(io.BytesIO(png_data)) as img:
-            display_image(img)
-        return True
+        image = Image.open(io.BytesIO(png_data))
+        return image
     except Exception as e:
-        print(f"Error converting SVG to PNG: {str(e)}")
-        return False
-    
+        raise Exception(f"Error converting SVG to PNG: {str(e)}")
 
 def generate_chessboard_image(board):
     # Create an empty chessboard image
     board_image = chess.svg.board(board=board)
-    svg_to_png(board_image)
+    return board_image
 
 # Example usage
 # uci_moves = "e8d7 a2e6 d7d8 f7f8"
@@ -56,4 +64,6 @@ initial_position_fen = "q3k1nr/1pp1nQpp/3p4/1P2p3/4P3/B1PP1b2/B5PP/5K2 b k - 0 1
 
 # san_moves, final_board = uci_to_san(uci_moves, initial_position_fen)
 san_moves, final_board = uci_to_san(uci_moves, initial_position_fen)
-generate_chessboard_image(final_board)
+board_image = generate_chessboard_image(final_board)
+png_image = svg_to_png(board_image)
+display_image(png_image)
