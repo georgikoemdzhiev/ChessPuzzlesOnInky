1d4b15435ee7e5d8e9c5a00b8482c3f53db20939
Georgi Koemdzhiev <koemdjiev@gmail.com>
2023-10-14 17:54:55 +0100

    changes

--- install_cron_job.sh
+++ install_cron_job.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # Get the directory of the Bash script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -12,13 +10,14 @@ PYTHON_CMD="python3 $SCRIPT_DIR/main.py"
 if [ "$(crontab -l 2>/dev/null | wc -l)" -eq 0 ]; then
     # No existing crontab, so create one
     echo "0 8 * * * $PYTHON_CMD" | crontab -
+    next_run="8:00 AM every day"
 else
     # User already has a crontab, just add the new job
     (crontab -l ; echo "0 8 * * * $PYTHON_CMD") | crontab -
+    next_run_time=$(date -d "$(crontab -l | grep "$PYTHON_CMD" | awk '{print $1, $2, $3, $4, $5}')")
+    next_run=$(date -d "$next_run_time" "+%H:%M %p on %A, %B %d, %Y")
 fi
 
-# Get the next scheduled run time
-next_run_time=$(date -d "$(crontab -l | grep "$PYTHON_CMD" | awk '{print $1, $2, $3, $4, $5}')" "+%A, %B %d, %Y %H:%M:%S")
-
+# Display confirmation and the next scheduled run time
 echo "Cron job successfully set up."
-echo "Next scheduled run: $next_run_time"
+echo "Next scheduled run: $next_run"
