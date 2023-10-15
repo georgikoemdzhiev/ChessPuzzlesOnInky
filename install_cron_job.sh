326b131e3f3abd179b6bfed1c0a88fafdf5c3a52
Georgi Koemdzhiev <koemdjiev@gmail.com>
2023-10-15 19:57:14 +0100

    Fix for fonts not openning correctly

--- install_cron_job.sh
+++ install_cron_job.sh
@@ -11,6 +11,7 @@ PYTHON_CMD="python3 $SCRIPT_DIR/main.py"
 # Check if the user has a crontab already
 if [ "$(crontab -l 2>/dev/null | wc -l)" -eq 0 ]; then
     # No existing crontab, so create one
+    # Change the working directory so that we can load the custom font
     echo "0 8 * * * cd $SCRIPT_DIR && $PYTHON_CMD" | crontab -
 else
     # User already has a crontab, just add the new job
