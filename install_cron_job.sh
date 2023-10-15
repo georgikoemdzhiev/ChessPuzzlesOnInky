add8c46b40f1b32ef8dbe60d1e233d7a69c3f0fb
Georgi Koemdzhiev <koemdjiev@gmail.com>
2023-10-15 20:02:47 +0100

    updated docs

--- install_cron_job.sh
+++ install_cron_job.sh
@@ -11,7 +11,7 @@ PYTHON_CMD="python3 $SCRIPT_DIR/main.py"
 # Check if the user has a crontab already
 if [ "$(crontab -l 2>/dev/null | wc -l)" -eq 0 ]; then
     # No existing crontab, so create one
-    # Change the working directory so that we can load the custom font
+    # Change the working directory so that we can load the custom fonts
     echo "0 8 * * * cd $SCRIPT_DIR && $PYTHON_CMD" | crontab -
 else
     # User already has a crontab, just add the new job
